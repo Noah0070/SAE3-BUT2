853c5f915ccacf7b36f733d6b26dd817379fe74b
Noah0070 <80363186+Noah0070@users.noreply.github.com>
2023-11-27 10:54:35 +0100

    Fonction readCAN et détection de ligne à distance

--- programme_arduino/programme_arduino.ino
+++ programme_arduino/programme_arduino.ino
@@ -5,6 +5,10 @@ DFRobot_ADS1115 ads(&Wire);
 int16_t adc0, adc1, adc2, adc3;
 int i = 1;
 
+int a0_line = 3400;
+int a1_line = 4050;
+int a2_line = 3900;
+
 void setup(void) 
 {
     Serial.begin(115200);
@@ -16,24 +20,33 @@ void setup(void)
     ads.init();
 }
 
+
+void readCAN() { // Fonction qui enregistre les valeurs du CAN dans les variables dédiées
+  adc0 = ads.readVoltage(0);
+  adc1 = ads.readVoltage(1);
+  adc2 = ads.readVoltage(2);
+  adc3 = ads.readVoltage(3);
+}
+
+
 void loop(void) 
 {
     if (ads.checkADS1115())
-    {
-        adc0 = ads.readVoltage(0);
+    {   
+        readCAN();
+        
         Serial.print("A0:");
         Serial.print(adc0);
         Serial.print("mV,  ");
-        adc1 = ads.readVoltage(1);
+        
         Serial.print("A1:");
         Serial.print(adc1);
         Serial.print("mV,  ");
-        adc2 = ads.readVoltage(2);
-        //adc2 = readCAN();
+        
         Serial.print("A2:");
         Serial.print(adc2);
         Serial.print("mV,  ");
-        adc3 = ads.readVoltage(3);
+        
         Serial.print("A3:");
         Serial.print(adc3);
         Serial.print("mV --> ");
@@ -43,27 +56,45 @@ void loop(void)
     {
         Serial.println("ADS1115 Disconnected!");
     }
-    i++;                   
+    i++;
+
+    if (detect_a0()) {
+      Serial.println("a0 -> ligne noire !");                    
+    }
+    if (detect_a1()) {
+      Serial.println("a1 -> ligne noire !");                    
+    }
+    if (detect_a2()) {
+      Serial.println("a2 -> ligne noire !");                    
+    }
     delay(500);
 }
 
-//int readCAN(void)
-//{
-//  bool first = true;
-//  int j = 0;
-//  adc2 = 0;
-//
-//  while (j < 50)  {
-//    adc2 += ads.readVoltage(2);
-//
-//    if (first == true) {
-//      first = false;
-//    }
-//    else {
-//      adc2 /= 2;
-//    }
-//    j++;
-//    delay(0.05);
-//  }
-//  return adc2;
-//}
+
+
+bool detect_a0(){
+  if (adc0 < a0_line) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+bool detect_a1(){
+  if (adc1 < a1_line) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+bool detect_a2(){
+  if (adc2 < a2_line) { // Si la ligne est détectée
+    return true;
+  }
+  else {
+    return false;
+  }
+}
