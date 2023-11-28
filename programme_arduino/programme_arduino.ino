37a68aa73465fd9c2fa03d18d404195ece9fab9c
Noah0070 <80363186+Noah0070@users.noreply.github.com>
2023-11-28 17:53:08 +0100

    intégration de certaines fonctions dans checkSensors

--- programme_arduino/programme_arduino.ino
+++ programme_arduino/programme_arduino.ino
@@ -8,9 +8,9 @@ DFRobot_ADS1115 ads(&Wire);
 int16_t adc0, adc1, adc2, adc3;
 int i = 1;
 
-char sensors = 0x0;
+//char sensors = 0x0;
 int a0_line = 3400;
-int a1_line = 4050;
+int a1_line = 4550;
 int a2_line = 3900;
 
 void setup(void) 
@@ -37,24 +37,24 @@ void loop(void)
 {
     if (ads.checkADS1115())
     {   
-        checkSensors();
+        //checkSensors();
         
-        Serial.print("A0:");
-        Serial.print(adc0);
-        Serial.print("mV,  ");
-        
-        Serial.print("A1:");
-        Serial.print(adc1);
-        Serial.print("mV,  ");
-        
-        Serial.print("A2:");
-        Serial.print(adc2);
-        Serial.print("mV,  ");
-        
-        Serial.print("A3:");
-        Serial.print(adc3);
-        Serial.print("mV --> ");
-        Serial.println(i);
+//        Serial.print("A0:");
+//        Serial.print(adc0);
+//        Serial.print("mV,  ");
+//        
+//        Serial.print("A1:");
+//        Serial.print(adc1);
+//        Serial.print("mV,  ");
+//        
+//        Serial.print("A2:");
+//        Serial.print(adc2);
+//        Serial.print("mV,  ");
+//        
+//        Serial.print("A3:");
+//        Serial.print(adc3);
+//        Serial.print("mV --> ");
+//        Serial.println(i);
     }
     else
     {
@@ -62,18 +62,27 @@ void loop(void)
     }
     i++;
 
-    checkSensors();
+    char sensors = checkSensors();
     Serial.println(sensors);
 
     
     if ((sensors&(1<<0)) > 0) {
-      Serial.println("a0 -> ligne noire !");                    
+      Serial.println("a0 -> LIGNE NOIRE !");                    
     }
+    else {
+      Serial.println("a0 -> aucune ligne");
+    }
+    
     if ((sensors&(1<<1)) > 0) {
-      Serial.println("a1 -> ligne noire !");                    
+      Serial.println("a1 -> LIGNE NOIRE !");                    
+    } else {
+      Serial.println("a1 -> aucune ligne");
     }
+    
     if ((sensors&(1<<2)) > 0) {
-      Serial.println("a2 -> ligne noire !");                    
+      Serial.println("a2 -> LIGNE NOIRE !");                    
+    } else {
+      Serial.println("a2 -> aucune ligne");
     }
 
     delay(500);
@@ -81,35 +90,62 @@ void loop(void)
 
 
 
-bool detect_a0(){
+//bool detect_a0(){
+//  if (adc0 < a0_line) {
+//    bit_set(sensors, 0);
+//  }
+//  else { 
+//    bit_clr(sensors, 0);
+//  }
+//}
+//
+//bool detect_a1(){
+//  if (adc1 < a1_line) {
+//    bit_set(sensors, 1);
+//  }
+//  else {
+//    bit_clr(sensors, 1);
+//  }
+//}
+//
+//bool detect_a2(){
+//  if (adc2 < a2_line) { // Si la ligne est détectée
+//    bit_set(sensors, 2);
+//  }
+//  else {
+//    bit_clr(sensors, 2);
+//  }
+//}
+
+char checkSensors(void) {
+  char sensors = 0x0;
+  adc0 = ads.readVoltage(0);
+  adc1 = ads.readVoltage(1);
+  adc2 = ads.readVoltage(2);
+  adc3 = ads.readVoltage(3);
+
+  // -------------------- détection capteur 0 -------------------------
   if (adc0 < a0_line) {
     bit_set(sensors, 0);
   }
   else { 
     bit_clr(sensors, 0);
   }
-}
 
-bool detect_a1(){
+  // -------------------- détection capteur 1 ---------------------------------
   if (adc1 < a1_line) {
     bit_set(sensors, 1);
   }
   else {
     bit_clr(sensors, 1);
   }
-}
 
-bool detect_a2(){
+  // -------------------- détection capteur 2 ------------------------------------------
   if (adc2 < a2_line) { // Si la ligne est détectée
     bit_set(sensors, 2);
   }
   else {
     bit_clr(sensors, 2);
   }
-}
-
-void checkSensors(void) {
-  detect_a0();
-  detect_a1();
-  detect_a2();
+  return sensors;
 }
