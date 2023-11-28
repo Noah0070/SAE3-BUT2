7bcdf2779e05c8b77eae2ff750e8e0b074e8c554
Noah0070 <80363186+Noah0070@users.noreply.github.com>
2023-11-28 16:55:57 +0100

    Partie commande opérationnelle

--- commande_robot/commande_robot.ino
+++ commande_robot/commande_robot.ino
@@ -1,14 +1,25 @@
+//Arduino PWM Speed Control：
+int E1 = 5;
+int M1 = 4;
+int E2 = 6;
+int M2 = 7;
 
-void setup() {
-  // Pin 9(OC1A) is output
-  pinMode(9, OUTPUT);
-  // Signal de sortie 30,5 Hz
-  OCR1A= 128;
-  // non-inverted fast PWM on OC1A with prescalar of 1
-  TCCR1A = (1<<COM1A1) | (1<<WGM11);
-  TCCR1B = (1<<WGM12) | (1<<CS10) | (1<<CS12);
+void setup()
+{
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(M1, OUTPUT);
+    pinMode(M2, OUTPUT);
+    pinMode(E1, INPUT);
+    pinMode(E2, INPUT);
+        // Signal de sortie 30,5 Hz
+    OCR1A= 105; // sert à régler le -alpha
+    // non-inverted fast PWM on OC1A with prescalar of 1
+    TCCR1A = (1<<COM1A0) | (1<<COM1A1) | (1<<WGM11) | (1<<WGM10); // Fast PWM inverting mode
+    TCCR1B = (1<<WGM12) | (1<<CS12) | (1<<CS10); // Fast PMW 10 bits - prescaler max (1024)
 }
 
-void loop() {
-  // Le code du programme principal peut être ajouté ici
+void loop()
+{
 }
