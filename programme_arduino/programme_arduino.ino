6b4cdcb99767e8f0cdaf2c91ebf986be1ca8db78
Noah0070 <80363186+Noah0070@users.noreply.github.com>
2023-12-12 15:32:03 +0100

    Modification du des paramètres du Timer 1 (PWM)

--- programme_arduino/programme_arduino.ino
+++ programme_arduino/programme_arduino.ino
@@ -7,14 +7,29 @@
 DFRobot_ADS1115 ads(&Wire);
 int16_t adc0, adc1, adc2, adc3;
 int i = 1;
+char sensors;
 
-//char sensors = 0x0;
-int a0_line = 3400;
-int a1_line = 4550;
-int a2_line = 3900;
+int E1 = 5;
+int M1 = 4;
+int E2 = 6;
+int M2 = 7;
+
+// ----------- REGLAGES SENSIBILITE CAPTEURS
+// VALEURS PLAQUE NON FIXEE
+//int a0_line = 3470;
+//int a1_line = 3850;
+//int a2_line = 4100;
+//int a3_line = 4250;
+
+// VALEURS PLAQUE FIXEE
+int a0_line = 3700;
+int a1_line = 3700;
+int a2_line = 4000;
+int a3_line = 3850;
 
 void setup(void) 
-{
+{   
+    // ----------------- SETUP CAN -----------------------------------
     Serial.begin(115200);
     ads.setAddr_ADS1115(ADS1115_IIC_ADDRESS1);   // 0x48
     ads.setGain(eGAIN_TWOTHIRDS);   // 2/3x gain
@@ -22,6 +37,22 @@ void setup(void)
     ads.setRate(eRATE_128);          // 128SPS (default)
     ads.setOSMode(eOSMODE_SINGLE);   // Set to start a single-conversion
     ads.init();
+
+    // ----------------- SETUP PARTIE PUISSANCE ROBOT ----------------------------
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(M1, OUTPUT);
+    pinMode(M2, OUTPUT);
+    pinMode(E1, INPUT);
+    pinMode(E2, INPUT);
+    pinMode(13, OUTPUT);
+        // Signal de periode 65ms
+    OCR1A= 200;
+    //ICR1 = 200;
+    OCR1B= 255; 
+    // non-inverted fast PWM on OC1A with prescalar of 1
+    TCCR1A = (1<<COM1A0) | (1<<COM1A1) | (1<<COM1B0) | (1<<COM1B1) | (1<<WGM11) | (1<<WGM10); // Fast PWM inverting mode -- On paramètre COM1A:B pour gérer les sorties
+    TCCR1B = (1<<WGM12) | (1<<CS12) | (1<<CS10); // Fast PMW 10 bits - prescaler max (1024)
 }
 
 
@@ -37,24 +68,24 @@ void loop(void)
 {
     if (ads.checkADS1115())
     {   
-        //checkSensors();
+        readCAN();
         
-//        Serial.print("A0:");
-//        Serial.print(adc0);
-//        Serial.print("mV,  ");
-//        
-//        Serial.print("A1:");
-//        Serial.print(adc1);
-//        Serial.print("mV,  ");
-//        
-//        Serial.print("A2:");
-//        Serial.print(adc2);
-//        Serial.print("mV,  ");
-//        
-//        Serial.print("A3:");
-//        Serial.print(adc3);
-//        Serial.print("mV --> ");
-//        Serial.println(i);
+        Serial.print("A0:");
+        Serial.print(adc0);
+        Serial.print("mV,  ");
+        
+        Serial.print("A1:");
+        Serial.print(adc1);
+        Serial.print("mV,  ");
+        
+        Serial.print("A2:");
+        Serial.print(adc2);
+        Serial.print("mV,  ");
+        
+        Serial.print("A3:");
+        Serial.print(adc3);
+        Serial.print("mV --> ");
+        Serial.println(i);
     }
     else
     {
@@ -62,8 +93,8 @@ void loop(void)
     }
     i++;
 
-    char sensors = checkSensors();
-    Serial.println(sensors);
+    sensors = checkSensors();
+    //Serial.println(i);
 
     
     if ((sensors&(1<<0)) > 0) {
@@ -85,37 +116,18 @@ void loop(void)
       Serial.println("a2 -> aucune ligne");
     }
 
+    if ((sensors&(1<<3)) > 0) {
+      Serial.println("a3 -> LIGNE NOIRE !");                    
+    } else {
+      Serial.println("a3 -> aucune ligne");
+    }
+
+    Serial.println(" ");
+    select_direction();
     delay(500);
 }
 
 
-
-//bool detect_a0(){
-//  if (adc0 < a0_line) {
-//    bit_set(sensors, 0);
-//  }
-//  else { 
-//    bit_clr(sensors, 0);
-//  }
-//}
-//
-//bool detect_a1(){
-//  if (adc1 < a1_line) {
-//    bit_set(sensors, 1);
-//  }
-//  else {
-//    bit_clr(sensors, 1);
-//  }
-//}
-//
-//bool detect_a2(){
-//  if (adc2 < a2_line) { // Si la ligne est détectée
-//    bit_set(sensors, 2);
-//  }
-//  else {
-//    bit_clr(sensors, 2);
-//  }
-//}
 
 char checkSensors(void) {
   char sensors = 0x0;
@@ -147,5 +159,70 @@ char checkSensors(void) {
   else {
     bit_clr(sensors, 2);
   }
+  // -------------------- détection capteur 3 ------------------------------------------
+  if (adc3 < a3_line) { // Si la ligne est détectée
+    bit_set(sensors, 3);
+  }
+  else {
+    bit_clr(sensors, 3);
+  }
   return sensors;
+}
+
+
+// ----------------- DETECTIONS DE LIGNE --------------------------------------------
+bool line_on_middle() {
+    if ((sensors&(1<<1)) > 0 || (sensors&(1<<2)) > 0) { // Si la ligne est détectée sur l'un des capteurs du milieu
+      return true;                    
+    } 
+    else {
+      return false;
+    }
+}
+
+bool line_on_right() {
+  if (!(line_on_middle()) && ((sensors&(1<<0)) > 0)) { // Si la ligne n'est pas détectée au milieu mais uniquement à droite
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+bool line_on_left() {
+  if (!(line_on_middle()) && ((sensors&(1<<3)) > 0)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+// ----------------- DIRECTION DU ROBOT -------------------------------------
+void go_forward(void) { // le robot avance tout droit (les deux moteurs fonctionnent)
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void turn_right(void) { // le robot s'arrête et tourne sur lui même vers la droite (moteur gauche désactivé)
+  pinMode(9, OUTPUT);
+  pinMode(10, INPUT);
+}
+
+void turn_left(void) { // le robot s'arrête et tourne sur lui même vers la gauche (moteur droit désactivé)
+  pinMode(9, INPUT);
+  pinMode(10, OUTPUT);
+}
+
+void select_direction(void) {
+  if (line_on_middle() || sensors == 0) {
+    go_forward();
+  }
+  else if (line_on_right()) {
+    turn_right();
+  }
+  else if (line_on_left()) {
+    turn_left();
+  }
 }
